28331d1ce85e5d6655cd4bce8ad7d79f45eaef5a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2025-11-09 18:00:29 +0000

    feat: adds adversary cards

--- src/data/threatActors.ts
+++ src/data/threatActors.ts
@@ -1,5 +1,9 @@
+import AdversaryA from "@assets/AdversaryA.png";
+import AdversaryB from "@assets/AdversaryB.png";
+import AdversaryC from "@assets/AdversaryC.png";
+
 export const threatActors = [
-  { name: "Actor A", countries: ["USA", "CAN", "GBR"] },
-  { name: "Actor B", countries: ["RUS", "CHN"] },
-  { name: "Actor C", countries: ["USA", "MEX", "BRA"] },
+  { name: "Actor A", countries: ["USA", "CAN", "GBR"], image: AdversaryA },
+  { name: "Actor B", countries: ["RUS", "CHN"], image: AdversaryB },
+  { name: "Actor C", countries: ["USA", "MEX", "BRA"], image: AdversaryC },
 ];
