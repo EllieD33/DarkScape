ca8ce9fb2cf204a05a5a3d8a969c74a316ce8a92
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2025-11-09 17:22:22 +0000

    feat: adds interactive map

---
+++ src/data/threatActors.ts
@@ -0,0 +1,5 @@
+export const threatActors = [
+  { name: "Actor A", countries: ["USA", "CAN", "GBR"] },
+  { name: "Actor B", countries: ["RUS", "CHN"] },
+  { name: "Actor C", countries: ["USA", "MEX", "BRA"] },
+];
